82a35d3a49eff888a80fbd41acca460c98262bc1
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-04 17:31:57 +0100

    Feature(Heroku):Deploy application to heroku

--- server/routes/roleRoutes.js
+++ server/routes/roleRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import RoleController from '../controllers/RoleController';
+import RoleController from '../controllers/roleController';
 import Authentication from '../middleware/authentication';
 
 const roleRouter = express.Router();
