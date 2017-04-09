568edf96bf2cabe702022cc63b56b560031e46a1
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-09 06:30:04 +0100

    feature(front-end)setup front end with all the neccessary packages

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-import dotenv from 'dotenv';
+import webpack from 'webpack';
+import path from 'path';
+import express from 'express';
+import open from 'open';
+import compression from 'compression';
 import app from './server/routes/index';
+import config from './webpack.config.dev';
 
-dotenv.config({ silent: true });
+const port = parseInt(process.env.PORT, 10) || 6060;
 
-const port = process.env.PORT || 6060;
+/* eslint-disable no-console */
+const env = (process.env.NODE_ENV || 'development') === 'development';
+if (env) {
+  const compiler = webpack(config);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+
+  app.use(require('webpack-hot-middleware')(compiler));
+} else {
+  app.use(express.static('dist'));
+  app.use(compression());
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
 
 app.listen(port, (error) => {
-  if (!error) {
-    console.log(`App listening on port ${port}...`);
-  } else {
-    console.log('error');
+  if (error) {
+    console.log(error);
   }
+  if (env) open(`http://localhost:${port}`);
+  console.log(`server running on port ${port}`);
 });
+
+export default app;
