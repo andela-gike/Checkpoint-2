e6f283a64c0ef04b787fe3c0c48e3dbbf0197286
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-10 05:39:29 +0100

    removing unwanted lines of code

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// import webpack from 'webpack';
-// import path from 'path';
-// import express from 'express';
-// import open from 'open';
-// import compression from 'compression';
-// import app from './server/routes/index';
-// import config from './webpack.config.dev';
-
-// const port = parseInt(process.env.PORT, 10);
-
-// /* eslint-disable no-console */
-// const env = (process.env.NODE_ENV || 'development') === 'development';
-// if (env) {
-//   const compiler = webpack(config);
-//   app.use(require('webpack-dev-middleware')(compiler, {
-//     noInfo: true,
-//     publicPath: config.output.publicPath
-//   }));
-
-//   app.use(require('webpack-hot-middleware')(compiler));
-// } else {
-//   app.use(express.static('dist'));
-//   app.use(compression());
-// }
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './client/index.html'));
-// });
-
-
-// app.listen(port, (error) => {
-//   if (error) {
-//     console.log(error);
-//   }
-//   if (env) open(`http://localhost:${port}`);
-//   console.log(`server running on port ${port}`);
-// });
-
-// export default app;
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import express from 'express';
@@ -48,9 +9,9 @@ import app from './server/routes/index';
 
 dotenv.config({ silent: true });
 
-const port = process.env.PORT || 6060;
+const port = process.env.PORT;
 
-app.use(express.static(path.join(__dirname, '/client')))
+app.use(express.static(path.join(__dirname, '/client')));
 
 const compiler = webpack(config);
 
