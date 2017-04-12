5e5f9566551e079144334ba8f7d315f6f72164d4
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-12 17:32:36 +0100

    Feature(Test):Add test for front-end

---
+++ client/tests/e2e/login.test.js
@@ -0,0 +1,60 @@
+/* eslint func-names: "off"*/
+/* eslint no-unused-vars: "off"*/
+const config = require('../../../nightwatch.conf.js');
+
+module.exports = {
+  'Document Management System': function (browser) {
+    browser
+      .url('http://localhost:6060/login')
+      .waitForElementVisible('body')
+      .assert.title('DoCMan')
+      .saveScreenshot('document-management-login.png')
+      .end();
+  },
+
+  'Login Users': function (browser) {
+    browser
+      .url('http://localhost:6060/login')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'admin')
+      .setValue('input[type=password]', 'adminkey')
+      .click('button[type="submit"]')
+      .waitForElementVisible('h4')
+      .assert.urlEquals('http://localhost:6060/login')
+      .end();
+  },
+
+  'Admin Dashboard Page': function (browser) {
+    browser
+      .url('http://localhost:6060/dashboard')
+      .waitForElementVisible('input[type=text]')
+      .setValue('input[type=text]', 'admin@example.com')
+      .setValue('input[type=password]', '123456')
+      .click('button[type="submit"]')
+      .waitForElementVisible('nav', 5000)
+      .assert.urlEquals('http://localhost:6060/login')
+      .assert.containsText('nav', 'Home')
+      .assert.containsText('nav', 'Saved Documents')
+      .assert.containsText('nav', 'Manage Users')
+      .assert.containsText('nav', 'Manage Roles')
+      .assert.containsText('nav', 'Logout')
+      .end();
+  },
+
+  'Regular Users Dashboard Page': function (browser) {
+    browser
+      .url('http://localhost:6060/dashboard')
+      .waitForElementVisible('body')
+      .setValue('input[type=text]', 'eseohe@gmail.com')
+      .setValue('input[type=password]', 'thequeeness')
+      .click('button[type="submit"]')
+      .waitForElementVisible('nav', 5000)
+      .assert.urlEquals('http://localhost:6060/login')
+      .assert.containsText('nav', 'Home')
+      .assert.containsText('nav', 'Saved Documents')
+      .assert.containsText('nav', 'Logout')
+      .assert.elementNotPresent('#admin')
+      .assert.cssClassNotPresent('nav', 'admin')
+      .end();
+  }
+};
